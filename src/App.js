cabcc0df03c79dddde14924c875ec404143c52c5
alfred <johnsonmessilo19@gmail.com>
2018-07-21 18:55:27 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Person from './Person/Person';
 import './App.css';
-
+import './Person/Person.css';
 class App extends Component {
+state = {
+    persons: [
+        {name: 'Max',age:50},
+        {name: 'Ozioma',age:18},
+        {name: 'Tom',age:30}
+    ]
+}
+swithNameHandler = (newName) =>{
+  //  console.log('Was Clicked')
+
+this.setState({persons:[
+
+    {name: newName,age:10},
+    {name: 'Ozioma',age:38},
+    {name: 'Tom',age:60}
+]})
+}
+
+changeName = (event) =>{
+
+this.setState({persons:[
+
+    {name: 'Fred',age:10},
+    {name: 'Ozioma',age:38},
+    {name: event.target.value,age:60}
+]})
+}
   render() {
+      const style ={
+          backgroundColor : 'White',
+          font: 'inherit',
+          border:'1px solid blue',
+          padding: '8px',
+          cursor:
+'Pointer',align:'center'
+      };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <h1>Hello I am  a React App!Working with States anad Props</h1>
+<Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+
+<Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+click={this.swithNameHandler.bind(this,'Alfred!')}/>
+
+<Person name={this.state.persons[2].name} age={this.state.persons[2].age}
+ changed={this.changeName}/>
+
+
+ <Person name="Tom" age="50">Yeah Baby!
+ <br/>
+ <button style={style} onClick={this.swithNameHandler.bind(this,'Alfred')}>Switch Name</button>
+
+ </Person>
+       </div>
     );
   }
 }
---
+++ src/Person/ProjectItems.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProjectItem extends Component {
+  render() {
+    return (
+      <div className="Projects">
+i am an extension project
+    {this.props.test}
+      </div>
+    );
+  }
+}
+
+export default ProjectItem;
